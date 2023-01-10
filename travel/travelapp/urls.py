4f8ac71a12537fbf3c10184cf92071e683be9b14
itsmedhanyamk <122359278+itsmedhanyamk@users.noreply.github.com>
2023-01-10 19:46:48 +0530

    Delete travel directory

--- travel/travelapp/urls.py
+++
@@ -1,9 +0,0 @@
-from . import views
-from django.urls import path
-
-urlpatterns = [
-    path('',views.demo,name='demo'),
-    path('about/',views.about,name='about'),
-    path('contact/',views.contact,name='contact'),
-    path('add/',views.addition,name='addition')
-]
